b08d3ab6d98d714ef23e6baa029582fbcf784849
ZhaozhongLiu <zl011@bucknell.edu>
2025-02-04 03:12:44 -0500

    updated back-end to listen on port 8080

--- cna-node-express/index.js
+++ cna-node-express/index.js
@@ -1,6 +1,6 @@
 //Creates an Express application server that listens on port 8080.
 const express = require('express')
-const port = process.env.PORT || 9090
+const port = process.env.PORT || 8080
 const app = express()
 app.use(express.json());
 app.listen(port, () => console.log(`Sample app is listening on port ${port}!`))
